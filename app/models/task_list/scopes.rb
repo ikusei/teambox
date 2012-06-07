449696ec14229910af6acb38ae8bad8aeabeb0ba
Marco Metz <marco.metz@gmail.com>
2012-06-07 18:53:22 +0200

    modified:   app/models/task_list/scopes.rb 	modified:   app/views/comments/_comment_form.html.erb

--- app/models/task_list/scopes.rb
+++ app/models/task_list/scopes.rb
@@ -1,5 +1,5 @@
 class TaskList
-  default_scope :order => 'position ASC, created_at DESC'
+  default_scope :order => 'task_lists.position ASC, task_lists.created_at DESC'
   scope :with_archived_tasks, :conditions => 'archived_tasks_count > 0'
   scope :archived, :conditions => {:archived => true}
   scope :unarchived, :conditions => {:archived => false}
